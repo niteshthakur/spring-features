180a126db7ac5f7b315b4ad72e882790652bd933
niteshthakur <nitesh@192.168.1.4>
2015-06-08 23:54:11 +0530

    routine

--- spring/src/main/java/spring/WebAppBoot.java
+++ spring/src/main/java/spring/WebAppBoot.java
@@ -5,16 +5,22 @@ import java.util.Set;
 import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import spring.web.servlets.MyServlet;
 
 public class WebAppBoot implements ServletContainerInitializer {
 
-	public void onStartup(Set<Class<?>> arg0, ServletContext arg1)
+	public void onStartup(Set<Class<?>> arg0, ServletContext context)
 			throws ServletException {
 		  System.out.println("===============================================");
 	        System.out.println("                 onStartup()                   ");
 	        System.out.println("===============================================");
 		System.out.println("TOMCAT found my initializer");
 		
+		Dynamic dynamic = context.addServlet("myServlet", MyServlet.class);
+		dynamic.addMapping("/api");
+		System.out.println("Added myServlet to context");
 	}
 
 }
